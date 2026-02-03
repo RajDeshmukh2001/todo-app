23701dbb0483936ffcdd7c1a6f56637fbd4c3477
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-03 15:20:04 +0530

    feat(service): add task service for task creation

---
+++ src/main/java/com/mvc/todo/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.mvc.todo.service;
+
+import com.mvc.todo.model.Priority;
+import com.mvc.todo.model.Task;
+import com.mvc.todo.repository.TaskRepository;
+
+public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(String title, String description, Priority priority) {
+        Task task = new Task(title, description, priority);
+        return taskRepository.saveTask(task);
+    }
+}
