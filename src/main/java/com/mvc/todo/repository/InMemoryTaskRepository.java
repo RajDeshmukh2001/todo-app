a6f11dc2588823027c1bab380ab9b1457a251944
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-04 23:00:13 +0530

    fix: add implementation to abstract method findAll in class InMemoryTaskRepository

--- src/main/java/com/mvc/todo/repository/InMemoryTaskRepository.java
+++ src/main/java/com/mvc/todo/repository/InMemoryTaskRepository.java
@@ -11,15 +11,20 @@ import java.util.Map;
 @Repository
 public class InMemoryTaskRepository implements TaskRepository {
     private final Map<String, Task> taskMap = new HashMap<>();
+
     @Override
     public Task save(Task task) {
         taskMap.put(task.getId(), task);
         return task;
     }
+
     @Override
     public boolean isExistsByTitle(String title) {
         return taskMap.values().stream().anyMatch(task -> task.getTitle().equalsIgnoreCase(title));
     }
 
-
+    @Override
+    public List<Task> findAll() {
+        return new ArrayList<>(taskMap.values());
+    }
 }
