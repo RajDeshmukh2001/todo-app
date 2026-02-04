9a1a72c8768e55e91cff44eceac7baadd6575ace
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-04 07:17:40 +0530

    feat(controller):add filter constraint(Priority,status) in getAllTasks()

--- src/main/java/com/mvc/todo/controller/TaskController.java
+++ src/main/java/com/mvc/todo/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.mvc.todo.controller;
 
 import com.mvc.todo.model.Priority;
+import com.mvc.todo.model.Status;
 import com.mvc.todo.model.Task;
 import com.mvc.todo.service.TaskService;
 import com.mvc.todo.validator.CreateTaskRequest;
@@ -24,7 +25,9 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<Task> getAllTasks() {
-        return taskService.getAllTasks();
+    public List<Task> getAllTasks(
+            @RequestParam(required = false) Status status,
+            @RequestParam(required = false) Priority priority) {
+        return taskService.getAllTasks(status, priority);
     }
 }
