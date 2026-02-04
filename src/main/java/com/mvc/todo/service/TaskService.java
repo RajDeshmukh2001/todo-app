983e7a7f9cc5d0851d213c2ccf9e5df65cc9af5f
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-04 22:57:53 +0530

    refactor: rename method getAllTasks to getTasks in TaskService

--- src/main/java/com/mvc/todo/service/TaskService.java
+++ src/main/java/com/mvc/todo/service/TaskService.java
@@ -12,21 +12,21 @@ import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
     public Task createTask(String title, String description, Priority priority) {
-        if (taskRepository.isExistsByTitle((title.trim().replaceAll("\\s+ ", " ")))) {
+        if (taskRepository.isExistsByTitle((title.trim().replaceAll("\\s+", " ")))) {
             throw new DuplicateTaskTitleException("Task with title '" + title + "' already exists.");
         }
         Task task = new Task(title, description, priority);
         return taskRepository.save(task);
     }
 
-    public List<Task> getAllTasks(Status status, Priority priority) {
+    public List<Task> getTasks(Status status, Priority priority) {
         return taskRepository.findAll().stream()
                 .filter(task -> status == null || task.getStatus() == status)
                 .filter(task -> priority == null || task.getPriority() == priority)
