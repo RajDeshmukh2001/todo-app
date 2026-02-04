15254f53a504008cc398059adba48273f035f910
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-04 13:51:37 +0530

    refactor: replace generic exception with DuplicateTaskTitleException

--- src/main/java/com/mvc/todo/service/TaskService.java
+++ src/main/java/com/mvc/todo/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.mvc.todo.service;
 
+import com.mvc.todo.exception.DuplicateTaskTitleException;
 import com.mvc.todo.model.Priority;
 import com.mvc.todo.model.Task;
 import com.mvc.todo.repository.TaskRepository;
@@ -15,7 +16,7 @@ public class TaskService {
 
     public Task createTask(String title, String description, Priority priority) {
         if (taskRepository.isExistsByTitle((title.trim().replaceAll("\\s+ ", " ")))) {
-            throw new IllegalArgumentException("Task with title '" + title + "' already exists.");
+            throw new DuplicateTaskTitleException("Task with title '" + title + "' already exists.");
         }
         Task task = new Task(title, description, priority);
         return taskRepository.save(task);
