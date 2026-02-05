a06a7d34b44621aeaafb34086184f6d2317881c4
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-05 09:29:23 +0530

    refactor: centralize text input validation logic in service

--- src/main/java/com/mvc/todo/service/TaskService.java
+++ src/main/java/com/mvc/todo/service/TaskService.java
@@ -1,9 +1,11 @@
 package com.mvc.todo.service;
 
 import com.mvc.todo.exception.DuplicateTaskTitleException;
+import com.mvc.todo.exception.TaskNotFoundException;
 import com.mvc.todo.model.Priority;
 import com.mvc.todo.model.Task;
 import com.mvc.todo.repository.TaskRepository;
+import com.mvc.todo.validator.UpdateTaskRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +16,19 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    public String validateTextInput(String input, String fieldName, int maxLength) {
+        String validTextInput = input.trim().replaceAll("\\s+", " ");
+        if (validTextInput.isBlank()) {
+            throw new IllegalArgumentException(fieldName + " is required and cannot be empty.");
+        }
+        if (validTextInput.length() > maxLength) {
+            throw new IllegalArgumentException(fieldName + " must not exceed " + maxLength + " characters.");
+        }
+        return validTextInput;
+    }
+
     public Task createTask(String title, String description, Priority priority) {
-        if (taskRepository.isExistsByTitle((title.trim().replaceAll("\\s+ ", " ")))) {
+        if (taskRepository.existsByTitle(validateTextInput(title, "Title", 100))) {
             throw new DuplicateTaskTitleException("Task with title '" + title + "' already exists.");
         }
         Task task = new Task(title, description, priority);
